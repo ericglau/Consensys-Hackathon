e2e1098ef87531b359830a12b817b304fd3c4a32
Ryan <drdgvhbh@gmail.com>
2019-08-17 18:07:38 -0400

    Create dumb oracle datasource

---
+++ contracts/TestDatasourceETHXBT.sol
@@ -0,0 +1,22 @@
+pragma solidity >= 0.5.0 < 0.6.0;
+
+contract TestDatasourceETHXBT {
+    string public value;
+    uint public lastUpdatedBlock;
+    bytes public what;
+
+    event OnUpdate();
+
+    constructor(bytes memory _what) public payable {
+      require(_what.length > 0, "what must not be empty");
+      what = _what;
+    }
+
+    function setValue(string memory _value) public {
+      value = _value;
+    }
+
+    function update() public payable {
+        emit OnUpdate();
+    }
+}
