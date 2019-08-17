97b5d25a6467eea97f44793c3dde8b0c35bc1f01
Ryan <drdgvhbh@gmail.com>
2019-08-17 17:13:09 -0400

    Update oracle interface

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -3,5 +3,5 @@ pragma solidity >= 0.5.0 < 0.6.0;
 interface Oracle {
     function value() external returns (string memory);
     function lastUpdatedBlock() external returns (uint);
-    function what() external returns (string memory);
+    function what() external returns (bytes memory);
 }
