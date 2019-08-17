871467afe0379850951b2e17ea82a52a14acda5f
Eric Lau <29640130+ericglau@users.noreply.github.com>
2019-08-17 18:48:43 -0400

    Move OracleSet to VouchedOracles

--- contracts/VouchedOracles.sol
+++ contracts/VouchedOracles.sol
@@ -2,6 +2,7 @@ pragma solidity >= 0.5.5 < 0.6.0;
 import "./Oracle.sol";
 
 contract VouchedOracles {
+    
     mapping(address => uint256) public oraclesBeingEvaluated;
     mapping(address => mapping(address => bool)) public voters;
     mapping(address => uint) public votes;
@@ -72,6 +73,7 @@ contract VouchedOracles {
             vouchedOraclesByWhat[oracle.what()].push(oracle);
             vouchedOracles[oracleAddr] = true;
             vouchedOracleIndex[oracleAddr] = vouchedOraclesByWhat[oracle.what()].length;
+            addOracle(oracleAddr);
         } else if (votes[oracleAddr] < 0) {
             if (!vouchedOracles[oracleAddr]) {
                 return;
@@ -82,6 +84,92 @@ contract VouchedOracles {
             vouchedOraclesByWhat[oracle.what()][index] = vouchedOraclesByWhat[oracle.what()][length-1];
             vouchedOraclesByWhat[oracle.what()].length--;
             vouchedOracleIndex[oracleAddr] = 0;
+            removeOracle(oracleAddr);
         }
+    }
+    
+    
+    
+    
+    
+    address[] oracles;
+    uint nonce;
+    
+    function addOracle(address oracleAddr) internal {
+        oracles.push(oracleAddr);
+    }
+    
+    function removeOracle(address oracleAddr) internal {
+        for (uint i=0; i<oracles.length; i++) {
+            if (oracles[i] == oracleAddr) {
+                removeOracleFromIndex(i);
+                break;
+            }
+        }
+    }
+
+    function removeOracleFromIndex(uint index) internal {
+        if (index < oracles.length) {
+            for (uint i=index; i<oracles.length-1; i++){
+                oracles[i] = oracles[i+1];
+            }
+            oracles.length--;
+        }
+    }
+
+    // get the array of all oracles
+    function getAllOracles() public view returns (address[] memory) {
+       return oracles;
+    }
+    
+    // get an array of oracles of given size, randomly
+    function getRandomOracles(uint size) public returns (address[] memory) {
+        if (size >= oracles.length) {
+            return oracles;
+        }
+        
+        address[] memory result = new address[](size);
+        int[] memory usedIndices = new int[](size);
+        // init array elements to -1
+        for (uint i = 0; i<size; i++) {
+            usedIndices[i] = -1;
+        }
+        
+        for (uint i = 0; i<size; i++) {
+            uint oracleIndex;
+            do {
+                oracleIndex = random();
+            } while(contains(usedIndices, int(oracleIndex)));
+            usedIndices[i] = int(oracleIndex);
+            result[i] = oracles[oracleIndex];
+        }
+        return result;
+    }
+ 
+    // pseudo random number generator
+    function random() public returns (uint) {
+        uint randomNumber = uint(keccak256(abi.encodePacked(now, msg.sender, nonce))) % oracles.length;
+        nonce++;
+        return randomNumber;
+    }
+    
+    // check if array contains the number
+    function contains(int[] memory array, int number) internal pure returns (bool) {
+        for (uint i = 0; i<array.length; i++) {
+            if (array[i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // check if array contains the address
+    function contains(address[] memory array, address addr) internal pure returns (bool) {
+        for (uint i = 0; i<array.length; i++) {
+            if (array[i] == addr) {
+                return true;
+            }
+        }
+        return false;
     }
 }
