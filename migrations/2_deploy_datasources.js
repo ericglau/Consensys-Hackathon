b1a80ef0600c3a55e16761031f5ec2f11474b013
Ryan <drdgvhbh@gmail.com>
2019-08-17 20:16:56 -0400

    Deploy test datasource contract

--- migrations/2_deploy_datasources.js
+++ migrations/2_deploy_datasources.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 const ProvableKrakenETHXBT = artifacts.require('ProvableKrakenETHXBT');
+const TestDatasourceETHXBT = artifacts.require('TestDatasourceETHXBT');
 
 module.exports = function(deployer) {
   deployer.deploy(
@@ -8,4 +9,5 @@ module.exports = function(deployer) {
     process.env.OraclizeAddrResolverI_Address,
     Buffer.from('ETHXBT'),
   );
+  deployer.deploy(TestDatasourceETHXBT, Buffer.from('ETHXBT'));
 };
